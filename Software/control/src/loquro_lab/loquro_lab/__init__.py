9bfec11b3ff6b9ae3a5aee240d1ab0ce8a21e0bf
GuilleMuac2 <guilleacosmuelas@gmail.com>
2026-05-30 12:34:10 +0200

    configured MJLab environment

--- Software/control/src/loquro_lab/loquro_lab/__init__.py
+++ Software/control/src/loquro_lab/loquro_lab/__init__.py
@@ -7,7 +7,7 @@ from .rl_cfg import quad_bot_ppo_runner_cfg
 
 # 2. Registramos la tarea
 register_mjlab_task(
-    task_id="quad_bot",
+    task_id="Mjlab-Velocity-Flat-LoQuRo",
     env_cfg=quad_bot_flat_env_cfg(play=False),
     play_env_cfg=quad_bot_flat_env_cfg(play=True),
     rl_cfg=quad_bot_ppo_runner_cfg(),
